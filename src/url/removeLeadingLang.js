7fbcee04707b1540651a62e24201bc5c3ddc47a0
Jonas Scheiwiller <git@jonasscheiwiller.ch>
2018-12-03 18:49:23 +0100

    [Update] removeLeadingLang: remove de/ as well, if de is not default language

--- src/url/removeLeadingLang.js
+++ src/url/removeLeadingLang.js
@@ -8,5 +8,5 @@
  */
 
 export default function removeLeadingLang (string) {
-  return string.startsWith('en/') || string.startsWith('fr/') || string.startsWith('it/') ? string.substr(3) : string
+  return string.startsWith('en/') || string.startsWith('fr/') || string.startsWith('it/') || string.startsWith('de/') ? string.substr(3) : string
 }
