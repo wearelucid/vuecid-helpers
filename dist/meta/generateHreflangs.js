6d9dacd5d684ce65a5af8eb9635af338c838499f
Marco Ehrenmann <marco.ehrenmann@wearelucid.ch>
2018-12-04 14:57:56 +0100

    [Update] Build: Build

--- dist/meta/generateHreflangs.js
+++ dist/meta/generateHreflangs.js
@@ -7,9 +7,11 @@ exports.default = generateHreflangs;
 
 var _removeHomeSlug = _interopRequireDefault(require("../url/removeHomeSlug"));
 
+var _verifyTrailingSlash = _interopRequireDefault(require("../url/verifyTrailingSlash"));
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-function generateHreflangs(post, url) {
+function generateHreflangs(post) {
   var hreflangs = []; // Only generate alternates if there is more than one language
 
   if (post.polylang && post.polylang.translations && post.polylang.translations.length > 1) {
@@ -17,7 +19,7 @@ function generateHreflangs(post, url) {
       var hreflang = {
         rel: 'alternate',
         hreflang: t.lang,
-        href: (0, _removeHomeSlug.default)(t.permalink)
+        href: (0, _verifyTrailingSlash.default)((0, _removeHomeSlug.default)(t.permalink))
       };
       hreflangs.push(hreflang);
     });
--- dist/meta/generateMetaImage.js
+++ dist/meta/generateMetaImage.js
@@ -5,10 +5,41 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.default = generateMetaImage;
 
-function generateMetaImage(options, post) {
-  // Get defaultImageUrl from options
-  var defaultImageUrl = options.localized.default_og_image ? options.localized.default_og_image.sizes['social-share-large'] : ''; // Override if OG enabled and image url
+function generateMetaImage() {
+  var _ref = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {},
+      _ref$siteSettings = _ref.siteSettings,
+      siteSettings = _ref$siteSettings === void 0 ? {} : _ref$siteSettings,
+      _ref$post = _ref.post,
+      post = _ref$post === void 0 ? {} : _ref$post;
 
-  var imageUrl = post.og_enabled && post.og_image && post.og_image.sizes['social-share-large'] ? post.og_image.sizes['social-share-large'] : defaultImageUrl;
-  return imageUrl;
+  // Throw if there is no default og image in the correct size
+  if (!siteSettings || !siteSettings.meta_og_image_default || !siteSettings.meta_og_image_default.sizes['social-share-large']) {
+    throw new Error('_siteSettings.meta_og_image_default.sizes[\'social-share-large\'] is required');
+  } // Get defaultImageUrl from site settings
+
+
+  var defaultImageUrl = siteSettings.meta_og_image_default.sizes['social-share-large']; // Override if set in post
+
+  var imageUrl = post.meta_og_image && post.meta_og_image.sizes['social-share-large'] ? post.meta_og_image.sizes['social-share-large'] : defaultImageUrl;
+  return [{
+    hid: 'og:image',
+    property: 'og:image',
+    content: imageUrl
+  }, {
+    hid: 'og:image:width',
+    name: 'og:image:width',
+    content: '1280'
+  }, {
+    hid: 'og:image:height',
+    name: 'og:image:height',
+    content: '720'
+  }, {
+    hid: 'twitter:image',
+    name: 'twitter:image',
+    content: imageUrl
+  }, {
+    hid: 'twitter:card',
+    name: 'twitter:card',
+    content: 'summary_large_image'
+  }];
 }
