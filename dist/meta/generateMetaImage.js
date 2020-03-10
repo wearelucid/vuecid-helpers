fc695fc30c5886ec808ffb084ee2d518ddc3a43b
Jonas Scheiwiller <git@jonasscheiwiller.ch>
2020-03-10 09:34:13 +0100

    [Update] OGImage: It is enough to have an og_image present to satisfy condition to override page wide SEO (no need for og_enabled)

--- dist/meta/generateMetaImage.js
+++ dist/meta/generateMetaImage.js
@@ -8,6 +8,6 @@ function generateMetaImage(options, post) {
   // Get defaultImageUrl from options
   var defaultImageUrl = options.localized.default_og_image ? options.localized.default_og_image.sizes['social-share-large'] : '';
   // Override if OG enabled and image url
-  var imageUrl = post.og_enabled && post.og_image && post.og_image.sizes['social-share-large'] ? post.og_image.sizes['social-share-large'] : defaultImageUrl;
+  var imageUrl = post.og_image && post.og_image.sizes['social-share-large'] ? post.og_image.sizes['social-share-large'] : defaultImageUrl;
   return imageUrl;
 }
